878a18f89b88fc7b50c1cf17d9de5750587b952d
Aleem Tariq <aleemtariq19@gmail.com>
2020-08-15 03:23:34 -0700

    finished available spots function.

--- board.py
+++ board.py
@@ -1,16 +1,19 @@
 class Board:
-    
+
     def __init__(self):
         self.board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
         self.score = 0
     
-    def winningBoard(self, board: Board) -> Boolean:
+    def winningBoard(self):
         pass
 
-    def availSpots(self, board: Board) -> List[int]:
+    def availSpots(self):
+        availSpots = []
+        for spot in self.board:
+            if spot != "X" and spot != "O":
+                availSpots.append(spot)
+        return availSpots
+
+    def drawBoard(self):
         pass
-
-    def drawBoard(self, board: Board):
-        pass
-
-
+    
