a8f12222587fe10cc037bdbb0a213f27e3b2d596
Aleem Tariq <aleemtariq19@gmail.com>
2020-08-15 04:18:37 -0700

    added draw board function.

--- board.py
+++ board.py
@@ -4,11 +4,16 @@ class Board:
         self.board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
         self.score = 0
     
-    def winningBoard(self):
+    def playerWin(self):
         wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
         for w in wins:
             if (self.board[w[0]]=="O" and self.board[w[1]]=="O" and self.board[w[2]]=="O"):
                 return True
+        return False
+    
+    def aiWin(self):
+        wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        for w in wins:
             if (self.board[w[0]]=="X" and self.board[w[1]]=="X" and self.board[w[2]]=="X"):
                 return True
         return False
@@ -24,4 +29,7 @@ class Board:
         self.board[index] = val
 
     def drawBoard(self):
-        pass
+        lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+        for l in lines:
+            print(str(self.board[l[0]]) + "  " + str(self.board[l[1]]) + "  " + str(self.board[l[2]]))
+
