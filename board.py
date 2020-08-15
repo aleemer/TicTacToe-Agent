2a5fffa0dd334b3ec140f729220786faf86a439c
Aleem Tariq <aleemtariq19@gmail.com>
2020-08-15 03:09:02 -0700

    basic board functionality

---
+++ board.py
@@ -0,0 +1,16 @@
+class Board:
+    
+    def __init__(self):
+        self.board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        self.score = 0
+    
+    def winningBoard(self, board: Board) -> Boolean:
+        pass
+
+    def availSpots(self, board: Board) -> List[int]:
+        pass
+
+    def drawBoard(self, board: Board):
+        pass
+
+
