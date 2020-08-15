8d7fb1a2e38852b1beb7c5c94cec24d170642f95
Aleem Tariq <aleemtariq19@gmail.com>
2020-08-15 03:51:52 -0700

    finished winning board check function.

--- board.py
+++ board.py
@@ -5,7 +5,13 @@ class Board:
         self.score = 0
     
     def winningBoard(self):
-        pass
+        wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        for w in wins:
+            if (self.board[w[0]]=="O" and self.board[w[1]]=="O" and self.board[w[2]]=="O"):
+                return True
+            if (self.board[w[0]]=="X" and self.board[w[1]]=="X" and self.board[w[2]]=="X"):
+                return True
+        return False
 
     def availSpots(self):
         availSpots = []
@@ -16,4 +22,3 @@ class Board:
 
     def drawBoard(self):
         pass
-    
